65dd244ffe94e4b655a205d667efcaa67f676b2d
gagan2125 <gagansk2125@gmail.com>
2024-01-20 14:40:28 +0530

    first commit

---
+++ app/Navbar.tsx
@@ -0,0 +1,39 @@
+'use client';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { FaBug } from "react-icons/fa";
+import classnames from 'classnames';
+
+const Navbar = () => {
+    const currentPath = usePathname();
+    console.log(currentPath);
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={classnames({
+                'text-zinc-1000': link.href === currentPath,
+                'text-zinc-500' : link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors':true
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
